40db619979e0f48f7e631b6815615aee693b89ee
johnkazer <30650393+johnkazer@users.noreply.github.com>
2019-08-08 09:05:10 +0100

    Update tests.js
    Provide 3 different implementation examples of the same test - useful view of how to structure the tests.

--- cypress/integration/tests.js
+++ cypress/integration/tests.js
@@ -1,25 +1,42 @@
 /// <reference types="cypress" />
 import { videoMachine, audioMachine } from '../../src/machines.js'
-import { getSimplePaths, getShortestPaths } from '@xstate/graph';
+import { getSimplePaths, getShortestPaths } from '@xstate/graph'
+import { map, compose, pipe } from 'ramda'
 
+// tests generated from the state machine path traversal graph(s)
 const videoSimplePaths = getSimplePaths(videoMachine)
 const audioSimplePaths = getSimplePaths(audioMachine)
 const videoShortestPaths = getShortestPaths(videoMachine)
 const audioShortestPaths = getShortestPaths(audioMachine)
 
 const stateList = Object.keys(videoSimplePaths)
-
-// test generated from the state machine path traversal
-it('tests each state path', () => {
+const checkTransition = ({ state, event }) => {
+    cy.log(`checking transition from ${state.value} due to ${event.type}`)
+}
+const listOfPaths = (currentState) => videoSimplePaths[currentState].paths[0]
+const checkPaths = (paths) => map(checkTransition, paths)
+it('tests each state path with forEach', () => {
     cy.visit('http://localhost:1234')
   
     stateList.forEach(currentState => {
-      videoSimplePaths[currentState].paths.forEach(arr => arr.forEach(({ state, event }) => {
-        cy.log(`checking transition from ${state.value} due to ${event.type}`)
-        //const selector = cy.get('button').contains('Take Picture').click()
-        //cy.get('img').should('have.length.gt', 0)
-      }))
-      cy.reload()
-    })
+		videoSimplePaths[currentState].paths.forEach(arr => arr.forEach(({ state, event }) => {
+			cy.log(`checking transition from ${state.value} due to ${event.type}`)
+		}))
+	})
 })
+it('tests each state path with map', () => {
+    cy.visit('http://localhost:1234')
   
+    map(currentState => {
+		map(arr => {
+			map(checkTransition, arr)
+		}, videoSimplePaths[currentState].paths)
+		cy.reload()
+    }, stateList)
+})
+
+it('tests each state path with compose', () => {
+	cy.visit('http://localhost:1234')
+	const test = map(compose(checkPaths, listOfPaths))
+	test(stateList)
+})
